6bf824167cf967315dedb27dd77bbe0869e82428
student-wroclaw <student-wroclaw@EDU-WSB.local>
2026-05-01 01:35:09 +0200

    makefile edit

--- hello_world/views.py
+++ hello_world/views.py
@@ -5,6 +5,7 @@ from flask import request
 
 moje_imie = "Andrzej"
 msg = "Hello World!"
+
 @app.route('/')
 def index():
     output = request.args.get('output')
@@ -12,6 +13,7 @@ def index():
         output = PLAIN
     return get_formatted(msg, moje_imie,
                          output.lower())
+                         
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
