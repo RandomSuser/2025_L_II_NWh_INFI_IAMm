9d9946aaaa849e668ef2e5e1287ad2cb398bc44c
student-wroclaw <student-wroclaw@EDU-WSB.local>
2026-05-01 01:18:07 +0200

    update

--- hello_world/views.py
+++ hello_world/views.py
@@ -5,7 +5,6 @@ from flask import request
 
 moje_imie = "Andrzej"
 msg = "Hello World!"
-
 @app.route('/')
 def index():
     output = request.args.get('output')
@@ -13,7 +12,6 @@ def index():
         output = PLAIN
     return get_formatted(msg, moje_imie,
                          output.lower())
-
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
